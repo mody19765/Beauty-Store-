e3bfc8260ce637e0aa36b4d848c06d5463189709
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-08-10 21:10:05 +0300

    Update Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -4,7 +4,7 @@ const customerSchema = new mongoose.Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phone_number: String,
+  phone_number: { type: String, required: true, unique: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Customer', customerSchema);
