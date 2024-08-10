f351e6478e2e0c5e2189997be0f536e4bb7d5b32
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-08-10 21:08:01 +0300

    Update Designer.js

--- models/Designer.js
+++ models/Designer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const designerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   Branch_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true },
-  phone_number: Number,
+  phone_number: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   specialization: String
 },
