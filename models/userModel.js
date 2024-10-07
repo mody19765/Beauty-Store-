55d5446956ce17ddd103bf0fe2c07d11f351fc21
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-10-07 09:47:56 +0300

    x

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'employee', 'media'], required: true },
   password: { type: String },
-  isPasswordSet: { type: Boolean, default: false }, // Track if the user has set a password
+  role: { type: String, enum: ['employee', 'media'], required: true },
+  isPasswordSet: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
