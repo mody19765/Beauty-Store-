60fb922813ab4fd9572db77832a44f3425e081a1
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-10-25 18:57:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
   });
-
-
+  app.use(cors()) 
 // Routes
 app.use('/', require('./routes/authRoutes')); // Auth routes
 app.use('/designers', authMiddleware.authenticateToken, require('./routes/designerRoutes'));
