29a1296db359c101bc8d8aa04cf318fdafa43e9e
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-10-25 16:51:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,31 +19,30 @@ connectDB();
 app.use(bodyParser.json());
 
 // CORS Setup
-// CORS Setup
-const allowedOrigins = ['http://localhost:3000', 'https://beauty-store-alpha.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://beauty-store-alpha.vercel.app', 'https://beauty-store-pi.vercel.app'];
 const corsOptions = {
   origin: function (origin, callback) {
-    // Allow requests with no `origin` (like mobile apps or Postman) or from `allowedOrigins`
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure OPTIONS is included for preflight
-  credentials: true, // Enable credentials like cookies
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'], // Extend allowed headers
-  preflightContinue: true, // Optionally allow preflight requests to pass through to middleware
-  optionsSuccessStatus: 200 // Provide a successful status for OPTIONS preflight
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
 
-// Explicitly handle preflight requests
+// Ensure preflight requests get a proper response
 app.options('*', cors(corsOptions));
-
-
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/authRoutes')); // Auth routes
