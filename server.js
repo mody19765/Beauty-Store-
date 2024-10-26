d948378c705636b93ec1eb36fa9eff7c4a3e7394
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-10-26 16:10:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const corsOptions = {
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Credentials":'true',
+        "Access-Control-Allow-Credentials":true,
         "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'"
   } 
 };
