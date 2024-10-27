6d273c8247e6d0e968f6eedc4d67c9bafa04eb0b
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-10-27 17:06:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(bodyParser.json());
 
 // CORS Setup
-const allowedOrigins = ['http://localhost:3000', 'https://beauty-store-alpha.vercel.app'];
+const allowedOrigins = ['https://beauty-store-pi.vercel.app'];
 const corsConfig = {
   credentials: true, // The Access-Control-Allow-Credentials header
   optionsSuccessStatus: 200, // Some legacy browsers choke on 204
