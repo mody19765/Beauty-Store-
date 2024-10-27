af96844061b9981c803a29e65cc3daf6fada2d10
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-10-27 22:31:42 +0300

    cv

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(cors(corsConfig));
 app.options('*', cors(corsConfig)); // Enabling CORS Pre-Flight
 // Routes
 app.use('/login', require('./routes/authRoutes')); // Auth routes
-app.use('/designers', authMiddleware.authenticateToken, require('./routes/designerRoutes'));
+app.use('/designers', require('./routes/designerRoutes'));
 app.use('/employees', authMiddleware.authenticateToken, require('./routes/employeeRoutes'));
-app.use('/sessions', require('./routes/sessionRoutes'));
+app.use('/sessions', authMiddleware.authenticateToken, require('./routes/sessionRoutes'));
 app.use('/services', authMiddleware.authenticateToken, require('./routes/serviceRoutes'));
 app.use('/branches', authMiddleware.authenticateToken, require('./routes/branchRoutes'));
 app.use('/customers', authMiddleware.authenticateToken, require('./routes/customerRoutes'));
