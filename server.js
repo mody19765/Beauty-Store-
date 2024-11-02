9dacd78fe66fd977a02f37b59b319c8f3f9853ce
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2024-11-02 18:57:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,16 @@ const corsConfig = {
     }
   }
 };
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+      return res.status(200).json({});
+  };
+  next();
+});
 app.use(cors(corsConfig));
 app.options('*', cors(corsConfig)); 
 // Enabling CORS Pre-Flight
